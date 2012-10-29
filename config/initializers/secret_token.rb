925f87c19dd81419615d7d44b49cbf3ecbecde2f
Emma <emma@snowbooks.com>
2012-10-29 17:59:15 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Snowbookssite::Application.config.secret_token = 'ffe02d2bf375b7dc69641e35fcc0b2a1c9f4a5f9511ef4622c4f461905f5c235e210d2cc7acf248d903584d6279ae0a5bbc2ec4cd9940828f26256e46d76c20b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Snowbookssite::Application.config.session_store :cookie_store, key: '_snowbookssite_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Snowbookssite::Application.config.session_store :active_record_store
